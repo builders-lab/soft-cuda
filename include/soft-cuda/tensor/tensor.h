14f2d759c7b123bb3ce4ffc289dbb87e72a99180
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-03-06 20:41:05 +0530

    Added memory free edit and some more functions

--- include/soft-cuda/tensor/tensor.h
+++ include/soft-cuda/tensor/tensor.h
@@ -58,6 +58,9 @@ uint32_t tensor_id(tensor_t *t);
  * @param dims        The shape of the tensor. Set to NULL to return a scalar
  *                    value, or provide an array of dimensions for a tensor, with
  *                    the last dimension followed by a zero.
+ *
+ * @param dtype       The data type of tensor objects.
+ * @param num_dims    The rank of the tensor 
  * @param elems       The elements of the tensor. The number of elements must
  *                    match the number of elements implied by the dimensions,
  *                    or can be NULL to create a tensor with zero values.
@@ -65,7 +68,17 @@ uint32_t tensor_id(tensor_t *t);
  *                    to insufficient memory in the pool.
  */
 
-tensor_t *tensor_create_float32(tensor_pool_t *pool, uint32_t *dims, float *elems);
+tensor_t *tensor_create(tensor_pool_t *pool,tensor_dtype_t dtype, uint32_t num_dims ,uint32_t *dims, float *elems);
+
+
+/*
+ * Establish a new memory arena
+ * @param capacity_bytes  The total size of the raw memory block to pre-allocate
+ * @return                Pointer to the opaque pool instance
+ */
+tensor_pool_t* tensor_pool_create(size_t capacity_bytes);
+
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // OPERATIONS
@@ -74,13 +87,20 @@ tensor_t *tensor_create_float32(tensor_pool_t *pool, uint32_t *dims, float *elem
 /*
  * Free the tensor
  *
- * @param tensor_id   Unique Id of the tensor to free
+ * @param pool   Address of the tensor to free
  *
  * @NOTE this API will be implemented in future for now it's just a place holder
  * We will be working with just BUMP allocator for now.
- * */
-void tensor_free(uint32_t tensor_id);
+ *
+ * Instantly invalidates all tensors in the pool by resetting the bump pointer to zero.
+ * Does NOT return memory to the OS. Highly efficient for the training loop.
+ */
+void tensor_pool_reset(tensor_pool_t *pool);
 
+/*
+ * Completely destroys the arena and returns the memory to the system.
+ */
+void tensor_pool_destroy(tensor_pool_t *pool);
 
 /*
  * Move the memory pool between device_type
@@ -118,6 +138,9 @@ void tensor_transpose(tensor_t *out, tensor_t *a);
 void tensor_add(tensor_t *out, tensor_t *x, tensor_t *y);
 
 
+// Explicit broadcasting for layers (e.g., Y = XW + b)
+void tensor_add_bias(tensor_t *out, const tensor_t *xw, const tensor_t *bias);
+
 /*
  * Do scalar matrix multiplication
  * @param out          Pointer to the tensor where result will be stored
@@ -137,6 +160,14 @@ void tensor_relu(tensor_t *out, tensor_t *a);
 // return how correct we were b/w 0-1
 tensor_t* tensor_mse_loss(tensor_t *predictions, tensor_t *target);
 
+// Fills an existing tensor with normally distributed random numbers.
+void tensor_fill_random_normal(tensor_t *t, float mean, float std_dev);
+
+// Fused operation combining Softmax and Cross-Entropy for stability
+tensor_t* tensor_cross_entropy_loss(const tensor_t *predictions, const tensor_t *targets);
+
+
+// API below it is still up in air but should be stable
 //////////////////////
 // THE BACKWARD PASS
 
@@ -144,4 +175,4 @@ tensor_t* tensor_mse_loss(tensor_t *predictions, tensor_t *target);
 void tensor_backward(tensor_t *loss_matrix);
 
 // The Optimizer
-void tensor_sgd_template(tensor_t *m, double learning_rate);
+void tensor_sgd_template(tensor_pool_t *static_weights_pool, double learning_rate);
